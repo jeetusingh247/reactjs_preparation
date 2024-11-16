9fe4eff89d26c73367ffdac91817b78c48e65234
jeetusingh247 <jeetusingh11010@gmail.com>
2024-11-16 11:44:34 +0530

    initialising Currency Converter

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-400'>Currency App</h1>
+    </>
+  )
+}
+
+export default App

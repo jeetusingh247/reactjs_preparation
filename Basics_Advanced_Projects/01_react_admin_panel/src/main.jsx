0d6d7e98e84f02ee6f4300cc32f791abc71c3731
jeetusingh247 <jeetusingh11010@gmail.com>
2025-02-02 22:22:25 +0530

    imported app.jsx and removed browser route

--- Basics_Advanced_Projects/01_react_admin_panel/src/main.jsx
+++ Basics_Advanced_Projects/01_react_admin_panel/src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import App from './App.jsx'; // Import the App component
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <StrictMode>
+        <App />
+    </StrictMode>
+);

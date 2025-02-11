e8b6c21b1813722be6d98636d3c97602fb4828c0
jeetusingh247 <jeetusingh11010@gmail.com>
2025-02-11 11:19:24 +0530

    modified app.jsx

--- Basics_Advanced_Projects/01_react_admin_panel/src/App.jsx
+++ Basics_Advanced_Projects/01_react_admin_panel/src/App.jsx
@@ -1,46 +1,31 @@
 import TopNavbar from "./scenes/global/TopNavbar.jsx";
 import { ColorModeContext, useMode } from "./theme.jsx";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Dashboard from "./scenes/Dashboard.jsx";
-import Team from "./scenes/Team.jsx";
-import Invoices from "./scenes/Invoices.jsx";
-import Contacts from "./scenes/Contacts.jsx";
-import Bar from "./scenes/Bar.jsx ";
-import Form from "./scenes/Form.jsx";
-import Line from "./scenes/Line.jsx";
-import Pie from "./scenes/Dashboard.jsx";
-import Faq from "./scenes/Faq.jsx";
-import Geography from "./scenes/Geography.jsx";
-import Calender from "./scenes/Calender.jsx";
+import Dashboard from "./scenes/dashboard/Dashboard.jsx";
+import { Routes, Route } from "react-router-dom";
+import SideNavbar from "./scenes/global/SideNavbar.jsx";
+import { useState } from "react";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <SideNavbar isSidebar={isSidebar} />
           <main className="content">
-            <TopNavbar />
+            <TopNavbar setIsSidebar={setIsSidebar} />
             <Routes>
-              <Route path="/" element={Dashboard}></Route>
-              <Route path="/Line" element={Line}></Route>
-              <Route path="/Pie" element={Pie}></Route>
-              <Route path="/Team" element={Team}></Route>
-              <Route path="/Bar" element={Bar}></Route>
-              <Route path="/Form" element={Form}></Route>
-              <Route path="/Invoices" element={Invoices}></Route>
-              <Route path="/Contacts" element={Contacts}></Route>
-              <Route path="/Faq" element={Faq}></Route>
-              <Route path="/Geo" element={Geography}></Route>
-              <Route path="/Calender" element={Calender}></Route>
+              <Route path="/" element={<Dashboard />} />
             </Routes>
           </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
-  )
+  );
 }
 
 export default App;

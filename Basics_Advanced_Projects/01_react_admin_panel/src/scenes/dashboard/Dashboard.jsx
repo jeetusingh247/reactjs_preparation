c01d73348584d2a99fa801406ce78cd8ec78df07
jeetusingh247 <jeetusingh11010@gmail.com>
2025-02-11 11:18:03 +0530

    init dashboard

--- Basics_Advanced_Projects/01_react_admin_panel/src/scenes/dashboard/Dashboard.jsx
+++ Basics_Advanced_Projects/01_react_admin_panel/src/scenes/dashboard/Dashboard.jsx
@@ -1,6 +1,14 @@
+import { Box } from "@mui/system";
+import Header from "../../components/Header";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>
+    return (
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignitem="center">
+                <Header title="Dashboard" subtitle="Welcome to React Admin Panel" />
+            </Box>
+        </Box>
+    )
 }
 
 export default Dashboard;
